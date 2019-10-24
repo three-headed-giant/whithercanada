b3edd5f1c663f03edb192cbe04484a4502c3438c
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2019-10-24 18:22:11 +0300

    hooks: pre-commit hook config #2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Whither Canada
-Project starter template for THG.
+Project starter template for THG and a bunch of pre-commit hooks.
--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="whitercanda",
-    author="thg",
-    version="0.1.0",
-    packages=["whitercanada"],
-)
+setup()
